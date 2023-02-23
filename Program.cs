264c2bbca63c274d8c3390e30dd54ac0598532a5
hp <hp@DESKTOP-4JI9J7D>
2023-02-23 21:37:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string FirstName, MiddleName, LastName, FullName;
+
+        Console.WriteLine("Please Enter First Name");
+        FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Middle Name");
+        MiddleName = Console.ReadLine();
+
+        Console.WriteLine("Please Enter Last Name");
+        LastName = Console.ReadLine();
+
+        FullName = FirstName + ' ' + MiddleName + ' ' + LastName;
+
+        Console.WriteLine(FullName);
+    }
+}

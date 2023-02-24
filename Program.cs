711a6de9df5d5942293187f51863c9b31dfad6a6
hp <hp@HPELITEBOOK840G>
2023-02-24 08:33:20 +0530

    String Manipulation

--- Program.cs
+++ Program.cs
@@ -1,20 +1,55 @@
-﻿public class Program
+﻿using System.Diagnostics.Metrics;
+
+public class Program
 {
     public static void Main()
     {
-        string FirstName, MiddleName, LastName, FullName;
+        //string FirstName, MiddleName, LastName, FullName;
 
-        Console.WriteLine("Please Enter First Name");
-        FirstName = Console.ReadLine();
+        //Console.WriteLine("Please Enter First Name");
+        //FirstName = Console.ReadLine();
 
-        Console.WriteLine("Please Enter Middle Name");
-        MiddleName = Console.ReadLine();
+        //Console.WriteLine("Please Enter Middle Name");
+        //MiddleName = Console.ReadLine();
 
-        Console.WriteLine("Please Enter Last Name");
-        LastName = Console.ReadLine();
+        //Console.WriteLine("Please Enter Last Name");
+        //LastName = Console.ReadLine();
 
-        FullName = FirstName + ' ' + MiddleName + ' ' + LastName;
+        //FullName = FirstName + ' ' + MiddleName + ' ' + LastName;
 
-        Console.WriteLine(FullName);
+        //Console.WriteLine(FullName);
+
+
+
+
+        //Calculate Length Of String
+        //Console.WriteLine("Please Enter Any String");
+        //var str = Console.ReadLine();
+
+        //int counter = 0;
+        //foreach (var item in str)
+        //{
+        //    counter++;
+        //}
+        //Console.WriteLine("Length of String=" +counter);
+
+
+
+        //Calculate Vowels From The String
+
+        Console.WriteLine("Please Enter Any String");
+        var str = Console.ReadLine();
+
+        int vowelsCounter = 0;
+        foreach (var item in str)
+        {
+            if (item == 'a' || item == 'e' || item == 'i' || item  == 'o' || item == 'u' ||
+                item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+            {
+                vowelsCounter++;  
+            }
+            
+        }
+        Console.WriteLine("Length of String=" +vowelsCounter);
     }
 }
